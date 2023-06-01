da44e1cbd1bf171eef7f3df3ffedaa9d67d0a075
Russell Gordon <rgordon@lcs.on.ca>
2023-06-01 14:13:03 -0400

    Initial Commit

---
+++ ColorSwatchExample/ColorSwatchExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSwatchExampleApp.swift
+//  ColorSwatchExample
+//
+//  Created by Russell Gordon on 2023-06-01.
+//
+
+import SwiftUI
+
+@main
+struct ColorSwatchExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
