e394e05ad450b29b461ea52dace51c79bc0dc586
Russell Gordon <rgordon@lcs.on.ca>
2023-06-01 14:22:37 -0400

    Example of generating a complementary color.

--- ColorSwatchExample/ContentView.swift
+++ ColorSwatchExample/ContentView.swift
@@ -8,12 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var hue: Double = 350.0
+    
+    var complementary: Double {
+        
+        let result = (hue + 180.0).remainder(dividingBy: 360.0)
+        print(result)
+        return result
+        
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Rectangle()
+                .foregroundColor(Color(hue: hue/360.0, saturation: 80.0/100.0, brightness: 90.0/100.0))
+                .frame(width: 200, height: 200)
+            Rectangle()
+                .foregroundColor(Color(hue: complementary/360.0, saturation: 80.0/100.0, brightness: 90.0/100.0))
+                .frame(width: 200, height: 200)
         }
         .padding()
     }
